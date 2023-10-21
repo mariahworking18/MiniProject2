4d5d241e4ca6042761989c8221ffb24d419be0af
imgibo <gibscreative@gmail.com>
2023-10-21 15:11:13 +0800

    Edit the background color

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,50 @@ import {
   SongDetails,
   TopCharts,
 } from "./pages";
+import { bgcolor, color, margin, maxHeight, maxWidth } from "@mui/system";
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { auto } from "@popperjs/core";
+
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
   return (
-    <div className="relative flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <Searchbar />
+    <Container maxWidth padding-0 margin-0>
+      <Box sx={{
+        width: maxWidth,
+        height: maxHeight,
+        margin: 0,
+        padding: 0,
+      }}>
+        <Sidebar />
+        <Box>
+          <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-          <div className="flex-1 h-fit pb-40">
-            <Routes>
-              <Route path="/" element={<Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
-              <Route path="/artists/:id" element={<ArtistDetails />} />
-              <Route path="/songs/:songid" element={<SongDetails />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
-            </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
-          </div>
-        </div>
-      </div>
+          <Box>
+              <Routes>
+                <Route path="/" element={<Discover />} />
+                <Route path="/top-artists" element={<TopArtists />} />
+                <Route path="/top-charts" element={<TopCharts />} />
+                <Route path="/around-you" element={<AroundYou />} />
+                <Route path="/artists/:id" element={<ArtistDetails />} />
+                <Route path="/songs/:songid" element={<SongDetails />} />
+                <Route path="/search/:searchTerm" element={<Search />} />
+              </Routes>
+            <div>
+              <TopPlay />
+            </div>
+          </Box>
+        </Box>
 
-      {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          <MusicPlayer />
-        </div>
-      )}
-    </div>
+        {activeSong?.title && (
+          <div>
+            <MusicPlayer />
+          </div>
+        )}
+    </Box>
+    </Container>
   );
 };
 
