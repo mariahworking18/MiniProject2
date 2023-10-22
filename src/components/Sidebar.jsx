af6a2a8d9cf227dee3722c7646b42f209db6f54d
imgibo <gibscreative@gmail.com>
2023-10-22 10:53:15 +0800

    adding nav links to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,60 +1,70 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {RiCloseLine} from 'react-icons/ri';
-
 import { links } from '../assets/constants';
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { Directions } from "@mui/icons-material";
 
 const drawerWidth = 240;
 
+const NavLinks = ({handleClick}) => (
+  <Box sx={{display: 'flex', flexDirection: 'column'}}>
+    {links.map((item) => (
+      <NavLink key={item.name} to={item.to} onClick={() => handleClick && handleClick()}>
+        {item.name}
+      </NavLink>
+    ))}
+  </Box>
+);
+
 const Sidebar = () => (
-  // export default function PermanentDrawerLeft() {
-    // return (
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
         <Drawer
           sx={{
+            // bgcolor: '#C0521D',
             width: drawerWidth,
             flexShrink: 0,
             '& .MuiDrawer-paper': {
               width: drawerWidth,
               boxSizing: 'border-box',
+              bgcolor: '#F4E1DE',
+              color: '#C0521D',
+              alignItems: 'center'
             },
           }}
           variant="permanent"
           anchor="left"
         >
+        <h1>GROOVE</h1>
         <Toolbar />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Discover', 'Around You', 'Top Artist', 'Top Charts'].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
+                <ListItemIcon sx={{color: '#A56B60'}}>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText sx={{color: '#A56B60'}} primary={text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
+        <NavLinks />
         </Drawer>
-      </Box>      
-    // );
-  // };      
+      </Box>          
 );
 
 export default Sidebar;
