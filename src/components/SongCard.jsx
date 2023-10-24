9fe438ff8377e325bb86bfa63400d8a89f3ec506
imgibo <gibscreative@gmail.com>
2023-10-24 19:56:06 +0800

    Tried to make a song card

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,18 @@
-const SongCard = () => (
-  <div>SongCard</div>
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { PlayPause} from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { Box, Container } from '@mui/material';
+
+const SongCard = ({ song, i }) => (
+  <Container sx={{display: 'flex', flexDirection: 'column', width: '250px', padding: '4', backgroundColor: 'white', borderRadius: '10px'}}>
+    <Box sx={{position: 'relative', width: 'full', height: '56'}}>
+      <Box sx={{position: 'absolute', justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray'}}>
+
+      </Box>
+    </Box>
+    
+  </Container>
 );
 
 export default SongCard;
